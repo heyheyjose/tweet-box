1a5739a943586f3be315a1f0881f8880e0b8316f
heyheyjose <heyheyjose@users.noreply.github.com>
2015-07-18 11:09:53 -0400

    setting up state on textarea to affect button

--- src/app.jsx
+++ src/app.jsx
@@ -2,18 +2,30 @@ var React = require('react');
 
 var TweetBox = React.createClass({
 
+	getInitialState: function () {
+		return {
+			text: ''
+		}
+	},
+
+	handleChange: function (event) {
+		this.setState({
+			text: event.target.value
+		});
+	},
+
 	render: function () {
 		return (
 			<div className="well clearfix">
-				<textarea className="form-control"></textarea>
+				<textarea className="form-control" onChange={this.handleChange}></textarea>
 				<br/>
 				<button className="btn btn-default pull-left">Add Photo</button>
-				<button className="btn btn-primary  pull-right">Tweet</button>
+				<button className="btn btn-primary  pull-right" disabled={this.state.text.length === 0}>Tweet</button>
 				<span className="char-count pull-right"></span>
 			</div>
 		);
 	}
-	
+
 });
 
 React.render(<TweetBox/>, document.getElementById('tweet-box'));
